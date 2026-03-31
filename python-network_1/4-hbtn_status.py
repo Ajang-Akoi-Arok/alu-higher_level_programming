f888e3bc3519163d5f2908920a27ea0a96f040e6
Ajang Biar <a.biar1@alustudent.com>
2026-03-31 18:24:41 +0300

    fixed version

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
-"""Script that fetches https://alu-intranet.hbtn.io/status"""
+"""Script that fetches https://alu-intranet.hbtn.io/status."""
 import requests
 
 
 if __name__ == "__main__":
+    # The test might replace this URL
     url = "https://alu-intranet.hbtn.io/status"
+    
+    # Don't modify the URL - just fetch whatever is given
     response = requests.get(url)
     content = response.text
     
