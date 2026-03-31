665ec6da1ea22ced0f633a551c34dce6da369c71
Ajang Biar <a.biar1@alustudent.com>
2026-03-31 18:28:31 +0300

    fixed version

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -4,13 +4,10 @@ import requests
 
 
 if __name__ == "__main__":
-    # The test might replace this URL
     url = "https://alu-intranet.hbtn.io/status"
-    
-    # Don't modify the URL - just fetch whatever is given
     response = requests.get(url)
     content = response.text
-    
+
     print("Body response:")
     print("\t- type: {}".format(type(content)))
     print("\t- content: {}".format(content))
