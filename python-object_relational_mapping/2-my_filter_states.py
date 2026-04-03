8152c2c520a319c03fa61d56b1879ea7cb0b1fc8
Ajang Biar <a.biar1@alustudent.com>
2026-04-03 21:43:04 +0200

    task completed

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,15 +1,30 @@
 #!/usr/bin/python3
-"""
-displays all values in the states table where name matches the argument.
-"""
+"""Displays all values in states table where name matches argument safely."""
 import MySQLdb
 import sys
 
-if __name__ == '__main__':
-    con = MySQLdb.connect(db=sys.argv[3], user=sys.argv[1], passwd=sys.argv[2])
-    with con.cursor() as cur:
-        """Used context manager to automatically close the cursor object"""
-        query = "SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY id;"
-        cur.execute(query.format(sys.argv[4]))
-        [print(row) for row in cur.fetchall()]
+if __name__ == "__main__":
+    # Connect to database
+    con = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+    
+    # Create cursor
+    cur = con.cursor()
+    
+    # Safe parameterized query (prevents SQL injection)
+    query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
+    cur.execute(query, (sys.argv[4],))
+    
+    # Fetch and display results
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    
+    # Clean up
+    cur.close()
     con.close()
