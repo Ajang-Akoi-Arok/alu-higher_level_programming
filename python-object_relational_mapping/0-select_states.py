97556c74b2b476dacbfa0cec460e35690ab9a029
Ajang Biar <a.biar1@alustudent.com>
2026-04-03 20:06:53 +0200

    task completed

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-"""
-Write a script that lists all states from the database hbtn_0e_0_usa
-"""
+"""Lists all states from database hbtn_0e_0_usa"""
 import MySQLdb
 import sys
 
-
 if __name__ == '__main__':
-    con = MySQLdb.connect(db=sys.argv[3], user=sys.argv[1], passwd=sys.argv[2])
-    with con.cursor() as cur:
-        """Used context manager to automatically close the cursor object"""
-        cur.execute('SELECT * FROM states ORDER BY states.id;')
-        [print(row) for row in cur.fetchall()]
-    con.close()
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
