9b65c32a458a4730ce0312544e2c3b26c7725c36
Ajang Biar <a.biar1@alustudent.com>
2026-04-03 21:39:37 +0200

    tast completed

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python3
-"""
-Lists all states with a name starting with N from the database hbtn_0e_0_usa
-"""
+"""Lists all states with name starting with N (upper N)."""
 import MySQLdb
 import sys
 
-
-if __name__ == '__main__':
-    con = MySQLdb.connect(db=sys.argv[3], user=sys.argv[1], passwd=sys.argv[2])
-    with con.cursor() as cur:
-        """Used context manager to automatically close the cursor object"""
-        cur.execute('SELECT * FROM states ORDER BY states.id;')
-        [print(row) for row in cur.fetchall() if row[1][0] == "N"]
+if __name__ == "__main__":
+    con = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+    cur = con.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
     con.close()
